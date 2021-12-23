d38f057de48a6d9d88e13d960bb953b3689d03c5
Mike Flynn <mike@thatmikeflynn.com>
2021-12-23 13:41:17 -0800

    Added shtudown script.

--- setup.sh
+++ setup.sh
@@ -7,9 +7,15 @@ sudo apt -y install python3-pip python3-pygame vim git
 cd /home/pi
 git clone https://github.com/mikeflynn/TheBatPhone.git batphone 
 
-# Start script on boot
+# Start scripts on boot
 crontab -l | grep '/home/pi/batphone/batphone.py' 1>/dev/null 2>&1
 if [[ $? == 1 ]]; then
 	line="@reboot python3 /home/pi/batphone/batphone.py"
+	(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
+fi
+
+crontab -l | grep '/home/pi/batphone/shutdown.py' 1>/dev/null 2>&1
+if [[ $? == 1 ]]; then
+	line="@reboot python3 /home/pi/batphone/shutdown.py"
 	(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 fi
